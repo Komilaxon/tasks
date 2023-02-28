7e953962f3017144ef439d7b83c57dae89a1d7ac
Komilaxon <fayziyevakomila94@gmail.com>
2023-02-28 23:54:30 +0500

    hometasks

---
+++ index.js
@@ -0,0 +1,27 @@
+//array ichidagi sonlarni juft va toqlarini alohida-alohida arrayga solish
+
+
+
+// let number = [1, 2, 3, 4, 5, 6, 7, 8,]
+
+// let number2 = [];
+// let number3 = [];
+
+// function a(){
+//     for(let i = 0; i < number.length; i++){
+//         if(number[i] % 2 == 0){
+//             number2.push (number[i]);
+//         }
+
+//         else if (number[i] % 2 !== 0){
+//             number3.push (number[i]);
+//         }
+//     }
+// }
+// a()
+//     console.log(number2);
+//     console.log(number3);
+
+
+
+
