ba57bf307f8b0357c6c27bc19fc532abedf24e0b
Komilaxon <fayziyevakomila94@gmail.com>
2023-03-01 11:19:46 +0500

    save changes

--- index.js
+++ index.js
@@ -1,27 +1,96 @@
 //array ichidagi sonlarni juft va toqlarini alohida-alohida arrayga solish
 
+// let num = [1, 2, 3, 4, 5, 6, 7, 8,]
 
-
-// let number = [1, 2, 3, 4, 5, 6, 7, 8,]
-
-// let number2 = [];
-// let number3 = [];
+// let num2 = [];
+// let num3 = [];
 
 // function a(){
-//     for(let i = 0; i < number.length; i++){
-//         if(number[i] % 2 == 0){
-//             number2.push (number[i]);
+//     for(let i = 0; i < num.length; i++){
+//         if(num[i] % 2 == 0){
+//             num2.push (num[i]);
 //         }
 
-//         else if (number[i] % 2 !== 0){
-//             number3.push (number[i]);
+//         else if (num[i] % 2 !== 0){
+//             num3.push (num[i]);
 //         }
 //     }
 // }
 // a()
-//     console.log(number2);
-//     console.log(number3);
+//     console.log(num2);
+//     console.log(num3);
 
 
+
+
+
+// array ichidagi eng katta sonni topish
+
+// let num = [5,6,8,18,9,15,];
+
+
+// function b(){
+//     let max = num[0];
+    
+//     for(let i = 0; i < num.length; i++){
+//         if(num[i] > max){
+//             max = num[i]
+//         }
+//     }
+
+//     return max;
+// }
+
+//     console.log(b());
+
+
+
+
+// array ichidagi eng kichik sonni topish
+
+// let num = [5,6,8,18,9,15,];
+
+
+// function b(){
+//     let max = num[0];
+    
+//     for(let i = 0; i < num.length; i++){
+//         if(num[i] < max){
+//             max = num[i]
+//         }
+//     }
+
+//     return max;
+// }
+
+//     console.log(b());
+
+
+
+
+
+// userlarning yoshi 18 dan katta bo'lsa consolega chiqarish
+
+// let person = [
+//     {name: 'Komila' , age: 29 , job: `student`},
+//     {name: `Zilola` , age: 20 , job: `student`},
+//     {name: `Durdona` , age: 18 , job: `student`},
+//     {name: `Muxlisa` , age: 15 , job: `student`},
+//     {name: `Diyora` , age: 30 , job: `student`}
+// ];
+
+// let arr = [];
+
+// function b(){
+//     for(let i = 0; i < person.length; i++){
+//         if (person[i].age > 18){
+//             arr.push(person[i]);
+//        }
+//     }
+
+//     return arr;
+// }
+
+// console.log(b());
 
 
